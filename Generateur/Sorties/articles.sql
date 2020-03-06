cdf874a8891e5fd279941630c0381da5763872ec
Kyyran <43873225+Kyyran@users.noreply.github.com>
2020-03-06 15:59:59 +0100

    Add files via upload

---
+++ Generateur/Sorties/articles.sql
@@ -0,0 +1,20 @@
+# generated via Exemples/articles.xml;
+
+DROP TABLE IF EXISTS articles;
+
+CREATE TABLE articles (
+    id VARCHAR(255)  ,
+    refArticle VARCHAR(255)   ,
+    PRIMARY KEY (`id`)
+) ; 
+
+INSERT INTO articles (id,refArticle) VALUES (1,'HYBPk-164') ;
+INSERT INTO articles (id,refArticle) VALUES (2,'BHDwm-519') ;
+INSERT INTO articles (id,refArticle) VALUES (3,'LFHti-009') ;
+INSERT INTO articles (id,refArticle) VALUES (4,'AJWcz-512') ;
+INSERT INTO articles (id,refArticle) VALUES (5,'ZFMCd-867') ;
+INSERT INTO articles (id,refArticle) VALUES (6,'SDZaU-484') ;
+INSERT INTO articles (id,refArticle) VALUES (7,'KWUgT-424') ;
+INSERT INTO articles (id,refArticle) VALUES (8,'QGPDQ-949') ;
+INSERT INTO articles (id,refArticle) VALUES (9,'SFBRY-643') ;
+INSERT INTO articles (id,refArticle) VALUES (10,'BVEVd-368') ;
